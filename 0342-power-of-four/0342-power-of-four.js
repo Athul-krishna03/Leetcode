c6a4d9869859af9951e288ccfa42ff8ab0ba738c
Athul krishna  P <86891323+Athul-krishna03@users.noreply.github.com>
2025-08-15 13:51:52 +0530

    Time: 0 ms (100%), Space: 57.5 MB (17.42%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfFour = function(n) {
+     if(n<=0) return false
+
+     let num = Math.log(n)/Math.log(4)
+     return Number.isInteger(num)
+};
